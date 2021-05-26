345bd5301a4e46b76d5acbb7a3b85ad8bb24a7a4
william-wash <53308387+william-wash@users.noreply.github.com>
2021-05-26 14:32:00 -0400

    Update put_command.sql
    Updated repo name

--- put_command.sql
+++ put_command.sql
@@ -1,5 +1,5 @@
 put
-    file:///Users/wwash/src/demo/ner_udf/target/scala-2.12/nlp-udf-assembly-1.0.jar
+    file:///Users/wwash/src/demo/nlp_udf/target/scala-2.12/nlp-udf-assembly-1.0.jar
     @util.func.java
     auto_compress = false
     overwrite = true
